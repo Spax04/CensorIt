a03e01d204364c505766f93b8dd7966711d425ec
Spax04 <gotlib14@gmail.com>
2024-02-18 22:30:09 +0200

    adding to white list works,probably white list of category wil not work,changed auth response, user block lvl setup is works

--- client/scripts/login.js
+++ client/scripts/login.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-
   const api = 'http://localhost:5000/auth'
   // Check if a token already exists in localStorage
   let token
@@ -10,46 +9,49 @@ document.addEventListener('DOMContentLoaded', function () {
       window.location.href = '../pages/config.html'
     }
   })
- 
 
   // login submit function
   document
     .getElementById('loginForm')
     .addEventListener('submit', async function (event) {
       event.preventDefault() // Prevent the default form submission
-  
+
       // Get the username and password from the form
       let username = document.getElementById('username').value
       let password = document.getElementById('password').value
-  
+
       // Make a fetch request to your API endpoint
       await fetch(`${api}/signIn`, {
-          method: "POST",
-          headers: {
-              "Content-Type": "application/json"
-          },
-          body: JSON.stringify({ email: {address:username}, password: password })
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: { address: username },
+          password: password
+        })
       })
-      .then(response => {
+        .then(response => {
           if (!response.ok) {
-              alert("Login failed. Please check your credentials and try again.");
+            alert('Login failed. Please check your credentials and try again.')
           }
-          return response.json();
-      })
-      .then(data => {
-          let token = data.token;
+          return response.json()
+        })
+        .then(data => {
+          let token = data.token
           let userId = data.userId
-          chrome.storage.local.set({ "token": token });
-          chrome.storage.local.set({ "userId": userId });
-  alert(userId)
-          window.location.href = "../pages/config.html";
-      })
-      .catch(error => {
-         
-          console.error("Error:", error);
-          alert("Here"+error.message);
-      });
-  
+          let personalBlockPercentage = data.personalBlockPercentage
+
+          chrome.storage.local.set({ token: token })
+          chrome.storage.local.set({ userId: userId })
+          chrome.storage.local.set({ personalBlockPercentage: personalBlockPercentage })
+
+          alert(personalBlockPercentage)
+          window.location.href = '../pages/config.html'
+        })
+        .catch(error => {
+          console.error('Error:', error)
+          alert('Here' + error.message)
+        })
     })
 })
-
